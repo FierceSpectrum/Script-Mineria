c3e8477854f719358db424666f785ab838efa674
Josafath17 <105898971+Josafath17@users.noreply.github.com>
2024-10-25 11:55:38 -0600

    Update Products View
    New columns were created

--- SQL/producto.sql
+++ SQL/producto.sql
@@ -1,23 +1,36 @@
 CREATE OR ALTER VIEW dim_products AS
 SELECT
    [product_id],
-    upper(product_name)as[product_name],
-    upper(category)as[category],
-    [unit_price]
+   [product_name],
+   [category], 
+   [unit_price],
+   [Units_In_Stock],
+   [quantity_per_unit],
+   [DB_ORIGIN]
 FROM
-    (         
-        SELECT CODIGO_PRODUCTO as [product_id], NOMBRE as [product_name],
-		CASE
-			WHEN GAMA = 'Aromáticas' THEN 'Aromatic Plants'
-			WHEN GAMA = 'Frutales' THEN 'Fruit Trees'
-			WHEN GAMA = 'Herramientas' THEN 'Tools'
-			WHEN GAMA = 'Ornamentales' THEN 'Ornamental Plants'
-			ELSE GAMA 
-		END as [category],
-		PRECIO_VENTA as [unit_price] from dbo.PRODUCTO
-        UNION
-        SELECT cast(ProductID as varchar(10)) ,ProductName,CategoryName,UnitPrice  from dbo.PRODUCTS as p
-        JOIN dbo.CATEGORIES as c  ON p.CategoryID = c.CategoryID 
-     ) as d;
+    (
+        SELECT 
+            CODIGO_PRODUCTO AS [product_id], 
+            NOMBRE AS [product_name],
+            GAMA AS [category],
+            TRY_CAST(PRECIO_VENTA AS DECIMAL(10,2)) AS [unit_price],
+            CAST(cantidad_en_STOCK AS VARCHAR(50)) AS [Units_In_Stock], 
+            'Unidad única' AS [quantity_per_unit] ,
+			cast('JARDINERIA' as nvarchar(10)) AS DB_ORIGIN
+        FROM dbo.PRODUCTO
+        UNION ALL
+        SELECT 
+            CAST(ProductID AS VARCHAR(10)) AS [product_id], 
+            ProductName AS [product_name], 
+            CategoryName AS [category],   
+            TRY_CAST(UnitPrice AS DECIMAL(10,2)) AS [unit_price],
+            CAST(UnitsInStock AS VARCHAR(50)) AS [Units_In_Stock], 
+            QuantityPerUnit AS [quantity_per_unit],'NORTHWIND' AS DB_ORIGIN
+        FROM dbo.PRODUCTS AS p
+        JOIN dbo.CATEGORIES AS c ON p.CategoryID = c.CategoryID
+    ) AS d;
+
+
+
 
 SELECT * FROM dim_products;
