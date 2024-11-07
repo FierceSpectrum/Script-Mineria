e9b6ba0bcf9a84ab0cc87bf23b8b411da17e9e06
Benjamín Sandí <127576883+FierceSpectrum@users.noreply.github.com>
2024-11-07 16:42:35 -0600

    create script

--- ETL/menu.py
+++ ETL/menu.py
@@ -1,13 +1,15 @@
 import scripts.extraccion_datos as dt
 import scripts.transformacion_vistas as vw
 import scripts.exportacion_datawarehouse as tb
+import scripts.warehouse as wh
 
 def menu_scripts():
     menu = """------ MENU ------
     1. Migrar a STAGING
     2. Crear Vistas
     3. Migrar vistas al WareHouse
-    4. Salir del menu
+    4. Montar tablas del WareHouse
+    5. Salir del menu
 
     Ingrese una opcion: ..."""
 
@@ -15,16 +17,20 @@ def menu_scripts():
 
     while True:
         opcion = input(menu)
-        if opcion in ("1","2","3"):
+        if opcion in ("1","2","3","4"):
             print("\n")
             if opcion == "1":
                 dt.migrate_oracle_to_sql()
                 dt.migrate_sql_to_sql()
             elif opcion == "2":
                 vw.clear_views()
-            else:
+            elif opcion == "3":
                 tb.migrate_view_to_table()
-        elif opcion == ("4"):
+            elif opcion == "4":
+                wh.create_tables_wh()
+            else:
+                pass
+        elif opcion == ("5"):
             break
         else:
             print("Ingrese una opcion valida")
--- ETL/warehouse.py
+++
@@ -1,8 +0,0 @@
-# import ETL.conexion_singleton as utl
-
-
-# def create_tables_wh(connection=utl.target_conn3):
-    
-
-# if __name__ == "__main__":
-    
